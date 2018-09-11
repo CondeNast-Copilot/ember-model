6816f809829c89a28fa86ae248766c20b3c634a0
Sean O'Hara <sohara@sohara.com>
2018-09-11 15:09:09 -0400

    Try to pass no-sandbox flag

--- tasks/options/qunit.js
+++ tasks/options/qunit.js
@@ -1,5 +1,10 @@
 module.exports = {
   cli: ['tests/index.html'],
+  puppeteer: {
+    args: [
+      "--no-sandbox"
+    ]
+  },
   development: {
     options: {
       urls: ['http://localhost:8000/tests/runner.html']
