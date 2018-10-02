a01adc67ffb876263385e7fa1d6391e78f3c1762
Dmitriy Korenblyum <dmitriy_korenblyum@condenast.com>
2018-10-02 13:38:53 -0400

    build

--- ember-model.js
+++ ember-model.js
@@ -772,7 +772,7 @@ Ember.Model = Ember.Object.extend(Ember.Evented, {
       }
     }
 
-    if (recursionDepth < 100) {
+    if (recursionDepth < 15) {
       recursionDepth++;
       for (let [key, meta] of this.constructor.relationships) {
         let data;
